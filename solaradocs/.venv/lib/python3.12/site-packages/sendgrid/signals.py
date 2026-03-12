3e408e9dcfcd825e17e0264461e9fcc94cb9c368
solaraengineer <solaradeveloper@gmail.com>
2026-03-12 10:58:43 +0100

    FInal commit

--- solaradocs/.venv/lib/python3.12/site-packages/sendgrid/signals.py
+++
@@ -1,4 +0,0 @@
-import django.dispatch
-
-sendgrid_email_sent = django.dispatch.Signal(providing_args=["message", "response"])
-sendgrid_event_recieved = django.dispatch.Signal(providing_args=["request"])
