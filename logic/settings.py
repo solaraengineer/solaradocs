334804f3e6f9654401c565a4338917cd77dee1e4
solaraengineer <solaradeveloper@gmail.com>
2026-03-31 14:07:06 +0200

    FInal commit

--- logic/settings.py
+++ logic/settings.py
@@ -81,15 +81,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'logic.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'django_db'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(env='DB_MAIN'),
 }
 
 AUTH_PASSWORD_VALIDATORS = [
