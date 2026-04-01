babbe46be6c5bf06545f17820972f23f5e8b10cb
solaraengineer <solaradeveloper@gmail.com>
2026-04-01 19:02:38 +0200

    FInal commit

--- solaradocs/views.py
+++ solaradocs/views.py
@@ -213,7 +213,7 @@ def dashboard(request):
 def setup(request):
     if request.method == 'GET':
         try:
-            user_tier = request.user.Tier
+            user_tier = request.user.tier
             tier_config = TIER_LIMITS.get(user_tier, TIER_LIMITS['free'])
             return render(request, 'setup.html', {'tier': user_tier, 'tier_config': tier_config})
         except Exception:
@@ -3359,8 +3359,6 @@ def redeem_invite_code(request):
     except Exception:
         logger.exception("redeem_invite_code failed")
         return JsonResponse({'success': False, 'error': 'Something went wrong'}, status=500)
-
-
 
 
 
